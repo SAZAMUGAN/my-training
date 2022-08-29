e0c5e8f9e4048e2009b40993025c2cb43e6228f4
SAZAMUGAN <hamagami1171@gmail.com>
2022-08-29 21:14:38 +0900

    アクションの追加

--- app/controllers/training_menus_controller.rb
+++ app/controllers/training_menus_controller.rb
@@ -1,4 +1,13 @@
 class TrainingMenusController < ApplicationController
   def index
+    # @training_menus = TrainingMenu.all
+  end
+
+  def new
+    # @training_menu = TrainingMenu.new
+  end
+
+  def create
+
   end
 end
