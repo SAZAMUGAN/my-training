4ba734594c3d42404076822e9f1ab07191921ae2
SAZAMUGAN <hamagami1171@gmail.com>
2022-08-29 20:04:03 +0900

    バリデーションとアソシエーションの編集

--- app/models/training_menu.rb
+++ app/models/training_menu.rb
@@ -1,4 +1,8 @@
 class TrainingMenu < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user 
   belongs_to :muscle_category
+
+  validates :name, :explanation, presence: true
+  validates :muscle_category_id, numericality: { other_than: 1 } 
 end
