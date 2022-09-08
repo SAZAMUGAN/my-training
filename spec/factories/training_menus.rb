04c93445e5ada4462254f82d59215979910c5a56
SAZAMUGAN <hamagami1171@gmail.com>
2022-09-08 22:43:42 +0900

    ※番外 トレーニングメニューのモデル単体テスト

--- spec/factories/training_menus.rb
+++ spec/factories/training_menus.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :training_menu do
+    name { Faker::Name.initials(number: 2) }
+    muscle_category                 { MuscleCategory.find_by_id 2 }
+    explanation              { Faker::Lorem.sentence}
+    association :user
+    
   end
 end
--- spec/models/training_menu_spec.rb
+++ spec/models/training_menu_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe TrainingMenu, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @training_menu = FactoryBot.build(:training_menu)
+  end
+  describe 'トレーニングメニュー登録' do
+    context '登録できるとき' do
+      it 'nameとmuscle_category_idとexplanationが存在すれば登録できる' do
+        expect(@training_menu).to be_valid
+      end
+    end
+
+    context '登録できないとき' do
+      it 'nameが空では登録できない' do
+        @training_menu.name = ''
+        @training_menu.valid?
+        expect(@training_menu.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'muscle_category_idが選択されてなければ登録できない' do
+        @training_menu.muscle_category_id = ''
+        @training_menu.valid?
+        expect(@training_menu.errors.full_messages).to include("Muscle category can't be blank")
+      end
+
+      it 'explanationが空では登録できない' do
+        @training_menu.explanation = ''
+        @training_menu.valid?
+        expect(@training_menu.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it 'userが紐づいてなければ登録できない' do
+        @training_menu.user = nil
+        @training_menu.valid?
+        expect(@training_menu.errors.full_messages).to include("User must exist")
+      end
+      
+    end
+  end
 end
