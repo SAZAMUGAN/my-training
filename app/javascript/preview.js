a683cb29bedcfff9e13dd0736737cda5e09bf60a
SAZAMUGAN <103872710+SAZAMUGAN@users.noreply.github.com>
2023-08-12 22:06:40 +0900

    見た目の変更

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   // 新規投稿・編集ページのフォームを取得
-  const trainingMenuForm = document.getElementById('new_training_menu');
+  const trainingMenuForm = document.getElementById('training_menu');
    // プレビューを表示するためのスペースを取得
    const previewList = document.getElementById('previews');
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
